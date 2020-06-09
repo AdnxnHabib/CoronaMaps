2f3e8495b82a7d4ee446970ae77d703409dac826
ADNXNHABIB <adnanhabib263@gmail.com>
2020-06-09 01:01:18 -0400

    organized and stored variable

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var map;
 
+var input; // input from search bar
+
 function initialize(){
     var mapOptions = {
         zoom: 10, center: {lat:44.660560, lng:-79.522947} 
@@ -16,5 +18,21 @@ function initialize(){
 
 }
 
-initialize();
+function initMap() {
+    // Create the initial Map
+      var middle = {lat:43.660560, lng:-79.522947};
+      var map = new google.maps.Map(
+          document.getElementById('map'), {zoom: 10, center: middle});
+        
+          // Center the map based on client's location
+          if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+     initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+     map.setCenter(initialLocation);
+ });
+}
+    }
 
+
+    input = document.getElementById("search").value;
+    console.log(input);
