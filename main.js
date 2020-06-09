6f0bdf8818f46bb9bdaaa5a350834bce023439f3
abnik519 <aritra.banik519@gmail.com>
2020-06-09 00:14:02 -0400

    Center on client's location

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function initMap(){
-    var middle = {lat:43.660560, lng:-79.522947};
-    var map = new google.maps.Map(document.getElementById('map'),{ zoom:10, center: middle});
+var map;
+
+function initialize(){
+    var mapOptions = {
+        zoom: 10, center: {lat:44.660560, lng:-79.522947} 
+    };
+    var map = new google.maps.map(document.getElementById('map'), mapOptions);
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+        });
+    }
+
 }
+
+initialize();
 
